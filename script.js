c6e2614d632a7af53fc6655c08c11edb0d957b66
Ihavegota1pencil <sjskaiaiuauzuxbbv333@gmail.com>
2024-05-27 16:35:03 +0300

    Add files via upload

---
+++ script.js
@@ -0,0 +1,28 @@
+// Fotoğraf dosyalarının listesi
+var photoList = [
+    "kedi/1.jpg",
+    "kedi/2.jpg",
+    "kedi/3.jpg",
+    "kedi/4.jpg",
+    "kedi/5.jpg",
+    "kedi/6.jpg",
+    "kedi/7.jpg",
+    "kedi/8.jpg",
+    "kedi/9.jpg",
+    "kedi/10.jpg",
+    "kedi/11.jpg",
+    // Diğer fotoğraflar
+];
+
+// Rasgele bir fotoğraf gösteren fonksiyon
+function showRandomPhoto() {
+    // Rastgele bir fotoğrafın indeksini seç
+    var randomIndex = Math.floor(Math.random() * photoList.length);
+    // Seçilen fotoğrafın dosya yolunu al
+    var randomPhoto = photoList[randomIndex];
+    // Seçilen fotoğrafı görüntüle
+    document.getElementById("displayed-image").src = randomPhoto;
+}
+
+// Düğmeye basıldığında rasgele fotoğrafı göster
+document.getElementById("show-random-button").addEventListener("click", showRandomPhoto);
